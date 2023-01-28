7a6ee9f28a14eaa574677cfaa1bfc75dad402f4d
I hate iu <101011776+finetglazer@users.noreply.github.com>
2023-01-28 21:32:10 +0700

    func

---
+++ git/func/def.py
@@ -0,0 +1,17 @@
+# def name(vars):
+#     lenh
+#     return or not 
+def hello():
+    print("hello")
+hello()
+def hello(a):
+    print(f"hello {a}")
+a="my love"
+hello(a)
+def giaithua(a):
+    tmp=1
+    for i in range(1,a+1):
+        tmp*=i
+    return tmp
+x=5
+print(giaithua(5))
